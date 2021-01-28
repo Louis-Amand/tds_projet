e01df0ce553b0ad64072cb72f3280013d77e0d6c
Louis-Amand <48965957+Louis-Amand@users.noreply.github.com>
2021-01-28 12:15:07 +0100

    Ajout du showMessage

--- app/controllers/TodosController.php
+++ app/controllers/TodosController.php
@@ -6,10 +6,15 @@ use Ubiquity\attributes\items\router\Post;
 use Ubiquity\attributes\items\router\Route;
 use Ubiquity\utils\http\USession;
 
+
 /**
  * Controller TodosController
  **/
 class TodosController extends ControllerBase{
+	const CACHE_KEY = 'datas/lists/';
+	const EMPTY_LIST_ID='not saved';
+	const LIST_SESSION_KEY='list';
+	const ACTIVE_LIST_SESSION_KEY='active-list';
 
 	public function initialize()
 	{
@@ -22,9 +27,28 @@ class TodosController extends ControllerBase{
 		$this->loadView('todosController/menu.html');
 	}
 
-	#[Route("_default", name:"home")]
-	public function index(){
+ 	#[Route(path:"_default", name:'home' )]
+ 	public function index(){
+	if(USession::exists(self::LIST_SESSION_KEY)) {
+		$list = USession::get(self::LIST_SESSION_KEY, []);
+		return $this->display($list);
+	}
+	$this->showMessage('Bienvenue !','TodoLists permet de gerer des listes...','info','info circle',[['caption'=>'ouvrir']]);
+ }
 	
+	public function display(array $list){
+		$this->loadView('todosController/display.html');
+	}
+
+
+	private function showMessage(string $header, string $message, string $type = '', string $icon = 'info circle',array $buttons=[]) {
+		$this->loadView('main/showMessage.html', compact('header', 'type', 'icon', 'message','buttons'));
+		
+	}
+
+	public function displayList(array $list){
+		$this->jquery->change('#multiple','$("._form").toggle();');
+		$this->jquery->renderView('todosController/displayList.html',['list'=>$list]);
 	}
 
 
